1bdc60d2c0685c86bf9f3e4db32041b2e8c9ae72
MrBoomDev <github@mrboomdev.ru>
2024-06-21 06:43:48 +0500

    Generate css files from scss ar runtime

--- routes/index.ts
+++ routes/index.ts
@@ -1,18 +1,32 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest } from "fastify";
 import getInternalizationMap from "../util/internalization";
+import { readFile } from "fs/promises";
 
 export default async function baseRoute(app: FastifyInstance) {
 	app.get("/", async (request, reply) => {
 		return reply.status(200)
-			.view("static/home.html", { i18n: getInternalizationMap(request) });
+			.view("web/home/index.html", { i18n: getInternalizationMap(request) });
 	});
 	
 	app.get("*", async (request, reply) => {
 		return reply.status(404)
-			.view("static/error.html", {
+			.view("web/error/index.html", {
 				title: "Page not found",
 				code: 404,
 				message: "Sorry, but the page you are looking for cannot be found. Please check the URL and try again."
 			});
+	});
+
+	app.get("/styles/:file", async (request: FastifyRequest<{ Params: { file: string } }>, reply) => {
+		const name = request.params.file;
+		const path = `generated/styles/${name.substring(0, name.lastIndexOf("."))}/styles.css`;
+
+		try {
+			return await readFile(path, "utf8");
+		} catch(e) {
+			return reply.status(404).send({ 
+				"error_message": `File not found! Tried path: ${path}`
+			});
+		}
 	});
 }
